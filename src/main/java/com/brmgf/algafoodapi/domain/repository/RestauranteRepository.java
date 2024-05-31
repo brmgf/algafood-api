ec990df6ba1492108bd59a5a7f3b1d10254d5204
Milene <milenefaria33@gmail.com>
2024-05-30 21:59:07 -0300

    resolvendo consulta n+1 de restaurantes

--- src/main/java/com/brmgf/algafoodapi/domain/repository/RestauranteRepository.java
+++ src/main/java/com/brmgf/algafoodapi/domain/repository/RestauranteRepository.java
@@ -2,8 +2,14 @@ package com.brmgf.algafoodapi.domain.repository;
 
 import com.brmgf.algafoodapi.domain.model.Restaurante;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
+
+    @Query("from Restaurante r join fetch r.cozinha")
+    List<Restaurante> findAll();
 }
