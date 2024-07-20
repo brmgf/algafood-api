61d8799c72bbe769df12a30e1542d705e545545e
Milene <milenefaria33@gmail.com>
2024-07-20 20:10:17 -0300

    testes de unidade cadastro de cozinha

--- src/test/java/com/brmgf/algafoodapi/AlgafoodApiApplicationTests.java
+++ src/test/java/com/brmgf/algafoodapi/AlgafoodApiApplicationTests.java
@@ -1,13 +1,64 @@
 package com.brmgf.algafoodapi;
 
+import com.brmgf.algafoodapi.domain.exception.EntidadeEmUsoException;
+import com.brmgf.algafoodapi.domain.exception.entidadenaoencontrada.CozinhaNaoEncontradaException;
+import com.brmgf.algafoodapi.domain.model.Cozinha;
+import com.brmgf.algafoodapi.domain.model.Restaurante;
+import com.brmgf.algafoodapi.service.cadastro.CadastroCozinhaService;
+import jakarta.validation.ConstraintViolationException;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 @SpringBootTest
-class AlgafoodApiApplicationTests {
+class CadastroCozinhaIntegrationTests {
+
+	@Autowired
+	private CadastroCozinhaService cadastroCozinhaService;
 
 	@Test
-	void contextLoads() {
+	void deveSalvarQuandoCadastrarCozinhaComDadosCorretos() {
+		Cozinha novaCozinha = new Cozinha();
+		novaCozinha.setNome("Chinesa");
+
+		novaCozinha = cadastroCozinhaService.salvar(novaCozinha);
+
+		Assertions.assertThat(novaCozinha).isNotNull();
+		Assertions.assertThat(novaCozinha.getId()).isNotNull();
 	}
 
+	@Test
+	void deveFalharQuandoCadastrarCozinhaSemNome() {
+		Cozinha novaCozinha = new Cozinha();
+
+		var exception = assertThrows(ConstraintViolationException.class,
+				() -> cadastroCozinhaService.salvar(novaCozinha));
+
+		Assertions.assertThat(exception).isNotNull();
+	}
+
+	@Test
+	void deveFalharQuandoExcluirCozinhaEmUso() {
+		Cozinha cozinha = new Cozinha();
+		cozinha.setId(1L);
+
+		Restaurante restaurante = new Restaurante();
+		restaurante.setCozinha(cozinha);
+
+		var exception = assertThrows(EntidadeEmUsoException.class,
+				() -> cadastroCozinhaService.remover(1L));
+
+		Assertions.assertThat(exception).isNotNull();
+	}
+
+	@Test
+	void deveFalharQuandoExcluirCozinhaInexistente() {
+		var exception = assertThrows(CozinhaNaoEncontradaException.class,
+				() -> cadastroCozinhaService.remover(999L));
+
+		Assertions.assertThat(exception).isNotNull();
+	}
 }
