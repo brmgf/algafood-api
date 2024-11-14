eab061ed8cd61fdbd39193ad109a9cf1a7cb442e
Milene <milenefaria33@gmail.com>
2024-11-13 21:23:08 -0300

    habilitando shallow etag

---
+++ src/main/java/com/brmgf/algafoodapi/core/WebConfig.java
@@ -0,0 +1,16 @@
+package com.brmgf.algafoodapi.core;
+
+import jakarta.servlet.Filter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public Filter shallowEtagHeaderFilter() {
+        return new ShallowEtagHeaderFilter();
+    }
+}
